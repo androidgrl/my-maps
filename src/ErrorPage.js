aae8a5142e22392a5dea711cf96933a57e6f2a02
Jamie Kawahara <kawaharajamie@gmail.com>
2018-12-23 19:31:58 -0800

    feature: Catch error and show error page

---
+++ src/ErrorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ErrorPage extends Component {
+  render() {
+    return (
+      <div>
+        Data could not load
+      </div>
+    )
+  }
+}
+
+export default ErrorPage;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ErrorPage from './ErrorPage';
 import * as serviceWorker from './serviceWorker';
 
-const googleKey = process.env.REACT_APP_GOOGLEKEY
-const foursquareId = process.env.REACT_APP_FOURSQUARE_ID
-const foursquareSecret = process.env.REACT_APP_FOURSQUARE_SECRET
+const googleKey = process.env.REACT_APP_GOOGLEKEY;
+const foursquareId = process.env.REACT_APP_FOURSQUARE_ID;
+const foursquareSecret = process.env.REACT_APP_FOURSQUARE_SECRET;
 
 fetch("https://api.foursquare.com/v2/venues/search?ll=-17.4887889,-149.9130473&v=20120610&client_id=" + foursquareId + "&client_secret=" + foursquareSecret)
   .then((response) => response.json())
   .then((json) => {
     const venues = json.response.venues.slice(0, 5);
     ReactDOM.render(<App url={"https://maps.googleapis.com/maps/api/js?v=3&key=" + googleKey + "&callback=initMap"} venues={venues} />, document.getElementById('root'));
-  })
+  }).catch((error) => ReactDOM.render(<ErrorPage />, document.getElementById('root')));
 
 serviceWorker.unregister();
