083b95cccf4dc82c26e88b2ba689021482d839d1
Jamie Kawahara <kawaharajamie@gmail.com>
2018-12-24 13:56:27 -0800

    feature: Add toggle sidebar functionality

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './SideBar.css';
 
 class SideBar extends Component {
+  toggleSidebar = () => {
+    const sidebar = document.getElementById('sidebar')
+    sidebar.classList.toggle('active');
+  }
+
   render() {
     return(
       <div id="sidebar">
-        <div class="toggle-button">
+        <div className="toggle-button" onClick={this.toggleSidebar}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-        <label for="location-search">Filter Locations</label>
+        <label htmlFor="location-search">Filter Locations</label>
           <input type="search" id="location-search" name="query" aria-label="Filter map locations"/>
         <button>Filter</button>
         <ul>
