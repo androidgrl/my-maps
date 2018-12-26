bee3900c71be22bb434fe0ac1cbd350cfd5afc2d
Jamie Kawahara <kawaharajamie@gmail.com>
2018-12-26 14:16:40 -0800

    feature: Improve error page styling

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './ErrorPage.css';
 
 class ErrorPage extends Component {
   render() {
     return (
-      <div>
+      <div className="error-message">
         Data could not load
       </div>
     )
